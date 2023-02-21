d0d300c0e074c3eca5c659aa7bee8704812b11f1
João Magalhães <joamag@gmail.com>
2023-02-21 23:42:17 +0000

    feat: improved type linting support

--- types/base.d.ts
+++ types/base.d.ts
@@ -11,11 +11,11 @@ interface APIInterface extends UserAPI, StoreAPI, RepairAPI, SaleSnapshotAPI {
 
 export declare class API extends BaseAPI implements APIInterface {
     ping(): Promise<object>;
-    
+
     selfUser(options: object): Promise<object>;
-    
+
     listStores(options: object): Promise<object[]>;
-    
+
     listRepairs(options: object): Promise<object[]>;
     createRepair(payload): Promise<object>;
     getRepair(objectId: number, options: object): Promise<object>;
